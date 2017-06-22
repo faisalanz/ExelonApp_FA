2bd88104d3ce87d1fb505c55c2c6b69b3a9f2bc1
faisalanz <faisal.ansari@gmail.com>
2017-06-22 04:48:56 -0700

    node server changes
    node server changes

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ var http = require('http')
 var https = require('https')
 var express = require('express')
 var bodyParser = require('body-parser')
+var httpProxy = require('express-http-proxy');
+var proxyConfig = require('./proxy-config.json');
+
+var apiForwardingUrl = proxyConfig.apiServer;
+
 var PORT = process.env.PORT || 8085
 var app = express()
+app.set('port', proxyConfig.port);
 app.use(bodyParser.json());
 app.use(express.static(__dirname))
+app.use('/css/libs/oj/v2.0.0/common', express.static(__dirname+'/css/libs/common'));
+
+console.log('Forwarding API requests to ' + apiForwardingUrl);
+
+server.all('/mobile/*', httpProxy(apiForwardingUrl, {
+    forwardPath: function(req, res) {
+      return require('url').parse(req.url).path;
+    }
+}));
 app.get('/', function (req, res) {
     res.send('index.html')
 });
